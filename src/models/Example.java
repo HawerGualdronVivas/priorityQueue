e064023890177e171c3c23f93da740e2ba46d406
Erik Molina <90419030+Biacuya@users.noreply.github.com>
2021-09-21 11:35:47 -0500

    Metodo agregar paciente
    Se creo el método para agregar un paciente.

--- src/models/Example.java
+++ src/models/Example.java
@@ -2,10 +2,24 @@ package models;
 
 public class Example {
 
+	private static final int SIZE_ARRAY = 10;
+
 	public Example() {
-		
+
 	}
-	public static void main(String[] args) {
-		
+
+	public Person[] patients = new Person[SIZE_ARRAY];
+
+	public boolean addPatient(Person pat) {
+		for (int i = 0; i < patients.length; i++) {
+			if (patients[i] != null) {
+				patients[i] = pat;
+
+				return true;
+			}
+		}
+		return false;
+
 	}
+
 }
