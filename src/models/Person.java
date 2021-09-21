5ac56d482295f2fea8c92707ce9292fd2be9cc51
HawerGualdronVivas <hawer.gualdron@uptc.edu.co>
2021-09-21 09:02:28 -0500

    Creacion de la clase persona, sus atributos y un comparador

---
+++ src/models/Person.java
@@ -0,0 +1,35 @@
+package models;
+
+public class Person implements Comparable<Person> {
+
+    private String name;
+    private int id;
+    private int age;
+
+    public Person(String name, int id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [age=" + age + ", id=" + id + ", name=" + name + "]";
+    }
+
+    @Override
+    public int compareTo(Person person) {
+        if(this.getAge() > person.getAge()) {
+            return 1;
+        } else if (this.getAge() < person.getAge()) {
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+
+}
